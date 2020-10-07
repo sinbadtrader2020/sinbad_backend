e74a86471f2bde626120ad82cbbec670adbb2d1b
shafiul kabir <shafiul@tirzok.com>
2020-10-07 19:22:52 +0600

    [1787] add view for company ascending order

--- db/sinbad-basic.sql
+++ db/sinbad-basic.sql
@@ -40,3 +40,22 @@ CREATE TABLE sf_user (
 
 CREATE OR REPLACE VIEW sf_user_view AS
 select id, first_name, last_name, email, street_address, city, country, mobile_number, language, user_create, zip_code, status from sf_user;
+
+CREATE OR REPLACE VIEW sf_companies_view AS
+SELECT
+  sf_company_id,
+  sf_act_symbol,
+  sf_company_name,
+  sf_security_name,
+  sf_exchange,
+  sf_cqs_symbol,
+  sf_etf,
+  sf_round_lot_size,
+  sf_test_issue,
+  sf_nasdaq_symbol,
+  sf_aaoifi_compliant,
+  sf_nc_reason,
+  sf_last_screened,
+  sf_created,
+  sf_updated
+  FROM sf_companies order by sf_act_symbol;
